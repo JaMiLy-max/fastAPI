d3be9be8c29a781035a33c05ca0db56babeab56d
Jamily <ajin20823@gmail.com>
2025-10-14 15:19:14 +0900

    Fixing code style with Black

--- fastapi_assignment/app/schemas/users.py
+++ fastapi_assignment/app/schemas/users.py
@@ -1,10 +1,12 @@
 from enum import Enum
 from pydantic import BaseModel, conint
 
+
 # gender 는 Enum 이며 ‘male’, ‘female’ 중에서만 선택가능 해야합니다.
 class GenderEnum(str, Enum):
     male = "male"
     female = "female"
+
 
 # FastAPI의 Pydantic 모델을 활용하여 저장할 데이터 검증을 수행
 class UserCreateRequest(BaseModel):
@@ -12,16 +14,18 @@ class UserCreateRequest(BaseModel):
     age: int
     gender: GenderEnum
 
+
 # FastAPI의 Pydantic 모델을 활용하여 수정할 데이터 검증을 수행
 class UserUpdateRequest(BaseModel):
     username: str | None = None
     age: int | None = None
 
+
 # FastAPI의 Pydantic 모델을 활용하여 검색할 데이터 검증을 수행
 class UserSearchParams(BaseModel):
     # username, age, gender 이외의 쿼리 매개변수는 에러 반환
-    model_config = {"extra":"forbid"}
+    model_config = {"extra": "forbid"}
 
     username: str | None = None
-    age: conint(gt=0) | None = None # age는 0보다 큰 값만 허용
+    age: conint(gt=0) | None = None  # age는 0보다 큰 값만 허용
     gender: GenderEnum | None = None
--- fastapi_assignment/main.py
+++ fastapi_assignment/main.py
@@ -5,62 +5,66 @@ from app.schemas.users import UserCreateRequest, UserUpdateRequest, UserSearchPa
 
 app = FastAPI()
 
-UserModel.create_dummy() # API 테스트를 위한 더미를 생성하는 메서드 입니다.
+UserModel.create_dummy()  # API 테스트를 위한 더미를 생성하는 메서드 입니다.
 
 
 # 1. 유저 생성 API 작성하기
-@app.post('/users')
+@app.post("/users")
 async def create_user(data: UserCreateRequest):
-	user = UserModel.create(**data.model_dump())
-	return user.id
+    user = UserModel.create(**data.model_dump())
+    return user.id
+
 
 # 2. 모든 유저 정보를 가져오는 API 작성하기
-@app.get('/users')
+@app.get("/users")
 async def get_all_users():
-	result = UserModel.all()
-	if not result:
-		raise HTTPException(status_code=404) # 에러명시. 유저정보 빈 경우
-	return result
+    result = UserModel.all()
+    if not result:
+        raise HTTPException(status_code=404)  # 에러명시. 유저정보 빈 경우
+    return result
+
 
 # 3. 유저 정보를 가져오는 API 작성하기
-@app.get('/users/{user_id}')
+@app.get("/users/{user_id}")
 async def get_user(user_id: int = Path(gt=0)):
-	user = UserModel.get(id=user_id)
-	if user is None:
-		raise HTTPException(status_code=404) # user_id에 해당하는 유저 객체가 없을 경우 404
-	return user
+    user = UserModel.get(id=user_id)
+    if user is None:
+        raise HTTPException(status_code=404)  # user_id에 해당하는 유저 객체가 없을 경우 404
+    return user
+
 
 # 4. 유저 정보를 부분 수정하는 API 작성하기
 @app.patch("/users/{user_id}")
 async def update_user(data: UserUpdateRequest, user_id: int = Path(gt=0)):
-	user = UserModel.get(id=user_id)
-	if user is None:
-		raise HTTPException(status_code=404)
-	user.update(**data.model_dump())
-	return user
+    user = UserModel.get(id=user_id)
+    if user is None:
+        raise HTTPException(status_code=404)
+    user.update(**data.model_dump())
+    return user
+
 
 # 5. 유저를 삭제하는 API 작성하기
 @app.delete("/users/{user_id}")
-async def delete_user(user_id: int = Path(gt=0)): # FastAPI의  Path객체로 검증
-	user = UserModel.get(id=user_id)
-	if user is None:
-		raise HTTPException(status_code=404)
-	user.delete()
+async def delete_user(user_id: int = Path(gt=0)):  # FastAPI의  Path객체로 검증
+    user = UserModel.get(id=user_id)
+    if user is None:
+        raise HTTPException(status_code=404)
+    user.delete()
 
-	return {'detail': f'User: {user_id}, Successfully Deleted.'}
+    return {"detail": f"User: {user_id}, Successfully Deleted."}
+
 
 # 6. 유저를 검색하는 API 작성하기
-@app.get('/users/search')
+@app.get("/users/search")
 async def search_users(query_params: UserSearchParams = Depends()):
-	valid_query = {key: value for key, value in query_params.model_dump(exclude_unset=True).items()}
-	filtered_users = UserModel.filter(**valid_query)
-	if not filtered_users:
-		raise HTTPException(status_code=404)
-	return 	filtered_users
+    valid_query = {key: value for key, value in query_params.model_dump(exclude_unset=True).items()}
+    filtered_users = UserModel.filter(**valid_query)
+    if not filtered_users:
+        raise HTTPException(status_code=404)
+    return filtered_users
 
 
+if __name__ == "__main__":
+    import uvicorn
 
-if __name__ == '__main__':
-	import uvicorn
-
-	uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
